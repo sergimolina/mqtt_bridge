0867be0df61662bd034168d6db069dcfda672043
FastPick User (marc@hanheide.net) <marc@hanheide.net>
2022-02-20 14:52:55 +0000

    added another hack for py3

--- src/mqtt_bridge/util.py
+++ src/mqtt_bridge/util.py
@@ -30,8 +30,23 @@ def monkey_patch_message_conversion():
         raise FieldTypeMismatchException(roottype, stack, rostype, msgtype)
     message_conversion._to_primitive_inst = _to_primitive_inst
 
+# trick from https://stackoverflow.com/questions/26130644/how-to-overcome-python-3-4-nameerror-name-basestring-is-not-defined to make basestring work in Py3
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+    monkey_patch_message_conversion()
 
-monkey_patch_message_conversion()
 extract_values = message_conversion.extract_values
 populate_instance = message_conversion.populate_instance
 
