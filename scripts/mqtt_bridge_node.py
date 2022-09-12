827427254199d513fd123f3f340ecd6ae0460ff9
Marc Hanheide <marc@hanheide.net>
2022-09-12 09:54:49 +0100

    don't overwrite configuration, was a bad idea

--- scripts/mqtt_bridge_node.py
+++ scripts/mqtt_bridge_node.py
@@ -9,20 +9,20 @@ from mqtt_bridge.app import MqttBridgeNode
 rospy.init_node('mqtt_bridge_node')
 
 # set the client_id to the mac address + something unique
-rospy.set_param(
-    '~mqtt/client/client_id',
-    (
-        rospy.get_name() 
-        + '_'
-        + ':'.join('%02x' % ((getnode() >> 8*i) & 0xff) for i in reversed(range(6)))
-        + '_' + str(uuid4())
-    )
-)
+# rospy.set_param(
+#     '~mqtt/client/client_id',
+#     (
+#         rospy.get_name() 
+#         + '_'
+#         + ':'.join('%02x' % ((getnode() >> 8*i) & 0xff) for i in reversed(range(6)))
+#         + '_' + str(uuid4())
+#     )
+# )
 
-rospy.set_param(
-    '~mqtt/client/clean_session', 
-    False
-)
+# rospy.set_param(
+#     '~mqtt/client/clean_session', 
+#     False
+# )
 
 
 node = MqttBridgeNode()
